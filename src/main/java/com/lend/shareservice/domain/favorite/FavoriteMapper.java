6ce2de07a5f32825d345d79df77bc9f9a47a515a
jonguk1 <whdddnr1245@naver.com>
2024-05-02 17:07:52 +0900

    favorite controller,service,serviceImpl,mapper,mapper.xml,user mappercontroller,service,serviceImpl,mapper,mapper.xml 기능 추가,myInterest.jsp목록보기 추가

--- src/main/java/com/lend/shareservice/domain/favorite/FavoriteMapper.java
+++ src/main/java/com/lend/shareservice/domain/favorite/FavoriteMapper.java
@@ -1,7 +1,15 @@
 package com.lend.shareservice.domain.favorite;
 
+import com.lend.shareservice.entity.Favorite;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface FavoriteMapper {
+    List<Favorite> favorites(Map<String, Object> map);
+
+    int getFavoriteTotalCount();
+
 }
--- src/main/java/com/lend/shareservice/domain/favorite/FavoriteService.java
+++ src/main/java/com/lend/shareservice/domain/favorite/FavoriteService.java
@@ -1,4 +1,14 @@
 package com.lend.shareservice.domain.favorite;
 
+import com.lend.shareservice.entity.Favorite;
+
+import java.util.List;
+
 public interface FavoriteService {
+
+    List<Favorite> favorites(int limit,int offset,String userId);
+
+    int getFavoriteTotalCount();
+
+
 }
