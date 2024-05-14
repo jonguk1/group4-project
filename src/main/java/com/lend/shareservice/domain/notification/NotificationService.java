aac68ff2124c1b9d3bbb8e60d9f1283301c1a9d0
Lee Jae Woong <114080852+ljwicdi@users.noreply.github.com>
2024-05-14 15:17:49 +0900

    글 수정

--- src/main/java/com/lend/shareservice/domain/notification/NotificationService.java
+++ src/main/java/com/lend/shareservice/domain/notification/NotificationService.java
@@ -1,6 +1,9 @@
 package com.lend.shareservice.domain.notification;
 
+import com.lend.shareservice.web.notification.dto.NotificationDTO;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.List;
 
 public interface NotificationService {
     SseEmitter subscribe(String id);
@@ -8,4 +11,6 @@ public interface NotificationService {
     void noti(String id, Object data);
 
     public void sendToClient(String id, Object data);
+
+    List<NotificationDTO> findNotificationByUserId(String userId);
 }
---
+++ src/main/java/com/lend/shareservice/web/notification/dto/NotificationDTO.java
@@ -0,0 +1,19 @@
+package com.lend.shareservice.web.notification.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class NotificationDTO {
+
+    private Integer notiId;
+    private String userId;
+    private String content;
+    private Date notiRegDate;
+
+
+
+}
